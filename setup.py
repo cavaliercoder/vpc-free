d67b479ac080220ada70595e5fec2d5da3c2a8f9
Ryan Armstrong <ryan@cavaliercoder.com>
2017-05-30 18:36:17 +0800

    Added auto-version to setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,26 @@
 distutils/setuptools install script.
 """
 
+import os
+import re
+
 from setuptools import setup
 
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+def get_version():
+    """
+    get_version reads the version info from bin/vpc-free:__version__
+    """
+
+    init = open(os.path.join(ROOT, 'bin', 'vpc-free')).read()
+    return VERSION_RE.search(init).group(1)
+
 setup(name='vpc-free',
-      version='1.0',
+      version=get_version(),
       description='Find free IP blocks in AWS EC2',
+      long_description=open('README.rst').read(),
       url='http://github.com/cavaliercoder/vpc-free',
       author='Ryan Armstrong',
       author_email='ryan@cavaliercoder.com',
